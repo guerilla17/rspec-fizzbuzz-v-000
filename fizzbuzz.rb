1c833d9894b06c1966ba8f650d2166ee2c2b46cd
Gigi M <mmgamao1@gmail.com>
2018-11-05 05:28:27 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -12,7 +12,6 @@ def fizzbuzz(int)
   elsif int % 5 == 0 # if the number int is divisible by 5
     "Buzz"  
     
-    
   else int % 4 == 0 # if the number int is not divisible by 3 or 5
     nil
 
